c564c4c93b058d4e1893a92a27cf2fd344630840
SachinKidambi <sachinkidambi@gmail.com>
2018-03-01 12:11:04 +0530

    a

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -7,4 +7,4 @@ mv SpringMVCSecurityXML.war sachin.war
 scp sachin.war sachin@54.244.209.186:./
 cd /usr/share/tomcat8/.jenkins/workspace/JavaApp/Scripts
 ssh sachin@54.244.209.186 < deploy.sh
-firefox http://54.244.209.186:8080/sachin
+
